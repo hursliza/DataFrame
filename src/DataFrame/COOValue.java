056c4b1a8efcf71d6d2a9af67d39409f1f311391
hursliza <lizavetahurskaya@gmail.com>
2019-10-24 01:00:48 +0200

    Value package Integer, Double and Float classes String class Date, Time classes DateTime class COOValue extends Value

--- src/DataFrame/COOValue.java
+++ src/DataFrame/COOValue.java
@@ -1,23 +1,76 @@
 package DataFrame;
 
-class COOValue {
+import value.*;
+import java.lang.String;
+import java.util.Objects;
+
+class COOValue extends Value{
     private int key;
     private Object value;
+
+    public String toString() {
+        return ("|\t(" + this.key + ", " + this.value + ")\t|");
+    }
+
+    public boolean eq(Value other) {
+        return (this.key == ((COOValue)other).key && this.value.equals(((COOValue) other).value));
+    }
+
+    public boolean neq(Value other) {
+        return (this != other);
+    }
+
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+        COOValue cooValue = (COOValue) other;
+        return key == cooValue.key &&
+                Objects.equals(value, cooValue.value);
+    }
 
     COOValue(int key, Object value){
         this.key = key;
         this.value = value;
     }
 
-    void printCOOValue(){
-        System.out.print("|\t(" + this.key + ", " + this.value + ")\t|");
-    }
-
     int getKey(){
         return this.key;
     }
 
-    Object getValue(){
+    Object getValue() {
         return this.value;
+    }
+
+    public int hashCode() {
+        return Objects.hash(this);
+    }
+
+    public COOValue returnValue() {
+        return this;
+    }
+
+    public Value add(Value other) {
+        return null;
+    }
+    public Value sub(Value other) {
+        return null;
+    }
+    public Value mul(Value other) {
+        return null;
+    }
+    public Value div(Value other) {
+        return null;
+    }
+    public Value pow(Value other) {
+        return null;
+    }
+    public boolean lte(Value other) {
+        return false;
+    }
+    public boolean gte(Value other) {
+        return false;
+    }
+    public Value create(String s) {
+        return null;
     }
 }
